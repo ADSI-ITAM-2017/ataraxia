873f3a0ad88b2241693d6c7fb1ce9fb359f3928a
jpfernandez11 <jp.fernandezc11@gmail.com>
2017-04-30 17:43:00 -0500

    Funcionalidad agregar usuario y casa

--- ataraxia/app/controllers/properties_controller.rb
+++ ataraxia/app/controllers/properties_controller.rb
@@ -8,8 +8,9 @@ class PropertiesController < ApplicationController
   end
 
   def create
-	
-	@property=Property.new(params[:property].permit(:name,:address,:description,:price,:available,:lesee,:distance,:elevator,:ground_floor,:pets_allowed,:furnished,:cleaning_service,:female_roomies,:male_rommies,:gym,:smoking_is_allowed,:user_id))
+
+	@property=Property.new(params[:properties].permit(:name,:address,:description,:price,:available,:lesee,:distance,:elevator,:ground_floor,:pets_allowed,:furnished,:cleaning_service,:female_roomies,:male_roomies,:gym,:smoking_is_allowed,:user_id))
+		
 	@property.save
 	redirect_to properties_path
   end
